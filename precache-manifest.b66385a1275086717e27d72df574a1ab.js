0a40bf940cfbacbd67aa54e12e6ac2a0234fa6c9
Mateusz Kaleta <m.kaleta9696@gmail.com>
2020-03-15 23:17:46 +0100

    Updates

---
+++ precache-manifest.b66385a1275086717e27d72df574a1ab.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e76a6e4ffb74a75e20fcb64154e16010",
+    "url": "/sportsCarsSPA/index.html"
+  },
+  {
+    "revision": "7fd3f25fe05f171934f7",
+    "url": "/sportsCarsSPA/static/css/main.2edffac2.chunk.css"
+  },
+  {
+    "revision": "671d9dbda678c3e82400",
+    "url": "/sportsCarsSPA/static/js/2.f1891adb.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/sportsCarsSPA/static/js/2.f1891adb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fd3f25fe05f171934f7",
+    "url": "/sportsCarsSPA/static/js/main.8bda2164.chunk.js"
+  },
+  {
+    "revision": "19a302200d9719400f64",
+    "url": "/sportsCarsSPA/static/js/runtime-main.6bb13635.js"
+  },
+  {
+    "revision": "7a03beb2b66bc239dac9fef53509823b",
+    "url": "/sportsCarsSPA/static/media/audirs7640.7a03beb2.jpg"
+  },
+  {
+    "revision": "48a75a0fcdbc4b66460a2c31d1d64162",
+    "url": "/sportsCarsSPA/static/media/aventador640.48a75a0f.jpg"
+  },
+  {
+    "revision": "e08b031d575d45468e9cb4e02ef71cec",
+    "url": "/sportsCarsSPA/static/media/bentley640.e08b031d.jpg"
+  },
+  {
+    "revision": "0d7c15e8852918bffc2bbe57e69f32a9",
+    "url": "/sportsCarsSPA/static/media/bmwm4640.0d7c15e8.jpg"
+  },
+  {
+    "revision": "ad8b9c849f031deb7d944525d672249c",
+    "url": "/sportsCarsSPA/static/media/city1920.ad8b9c84.jpg"
+  },
+  {
+    "revision": "b5738af86824c3b43d57fa197f650b71",
+    "url": "/sportsCarsSPA/static/media/homeav640.b5738af8.jpg"
+  },
+  {
+    "revision": "be6c0f5d8610eadf58844d76bcfe22d7",
+    "url": "/sportsCarsSPA/static/media/mclaren640.be6c0f5d.jpg"
+  },
+  {
+    "revision": "b2d4402bbf5475221cc98a6ba4100180",
+    "url": "/sportsCarsSPA/static/media/mercedes640.b2d4402b.jpg"
+  },
+  {
+    "revision": "92631df648ea10ef2f6616570380ac00",
+    "url": "/sportsCarsSPA/static/media/mustang640.92631df6.jpg"
+  },
+  {
+    "revision": "b7872de56afd83015834777adc76b3bc",
+    "url": "/sportsCarsSPA/static/media/porsche-911640.b7872de5.jpg"
+  }
+]);
